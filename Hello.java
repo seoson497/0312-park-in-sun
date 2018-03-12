bd0507cc6deb81021861e49135ea42e7a9447371
seoson497 <37062271+seoson497@users.noreply.github.com>
2018-03-12 22:22:00 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요?"); //1
+		System.out.println("자바를 처음 공부합니다.");
+		System.out.println("자바는 재미있나요?");
+		int x=100;
+		x++;
+		x++;
+		System.out.println(x);
+	}
+
+}
